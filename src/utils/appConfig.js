fe4efc7f513e560f0faa62f678e4c28659aa8fbe
macro <zhenghong1991@126.com>
2025-01-13 10:37:03 +0800

    使用vue-cli重新构建项目结构

--- src/utils/appConfig.js
+++ src/utils/appConfig.js
@@ -1,6 +1,6 @@
 // appConfig.js
 
 //配置API请求的基础路径
-export const API_BASE_URL = 'http://localhost:8085';
+export const API_BASE_URL = 'https://portal-api.macrozheng.com';
 //是否启用支付宝支付
 export const USE_ALIPAY = false;
